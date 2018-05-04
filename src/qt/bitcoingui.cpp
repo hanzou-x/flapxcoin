729e921dcc90f75192c55afc3444e394891384b6
user <flapmin@gmail.com>
2018-05-04 12:41:41 -0400

    UI

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -94,7 +94,7 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     nWeight(0)
 {
 
-    setFixedSize(930, 620);
+    setFixedSize(930, 625);
     setWindowTitle(tr("FlapXCoin") + " - " + tr("Rev")+" - " + tr("1.1"));
 #ifndef Q_OS_MAC
     qApp->setWindowIcon(QIcon(":icons/bitcoin"));
